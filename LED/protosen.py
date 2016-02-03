172bc5faa31bfcfe10de68c4973d861b3a732eb1
Superchedz <grahamchedzoy@icloud.com>
2016-02-03 16:04:13 +0000

    typo

--- LED/protosen.py
+++ LED/protosen.py
@@ -399,7 +399,7 @@ while 1 :
      while buf[0] !='a':
        llapMsg = ser.read(12)
        print llapMsg
-	   print now
+       print now
 #  display packet, helps to troubleshoot any errors
        now = datetime.datetime.now()
        now.strftime("%H:%M %m-%d-%Y")
