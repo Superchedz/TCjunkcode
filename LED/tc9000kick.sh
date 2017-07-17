d4ea6ce5df9b10eb913c65710ceadfec9acfa53a
Superchedz <grahamchedzoy@icloud.com>
2017-07-17 13:50:49 +0100

    called kicker from tc9000kick

--- LED/tc9000kick.sh
+++ LED/tc9000kick.sh
@@ -1,6 +1,8 @@
 echo "##########################################################################"
 echo "################## Starting TC9000 system jobs ###########################"
 echo "##########################################################################"
+echo "This job now just does the GIT pull, to allow the install to run in one go"
+echo "After this, kicker.sh runs to do any install work"
 
 sleep 30
 echo
@@ -26,84 +28,5 @@ echo
 sudo chmod +x *.sh
 sudo chown pi:root *.sh
 
-
-
-echo ############### Shutdown installer #####################
-echo ## we can always do this as if the row exists it will just give an error.
-echo created 16/7/2017
-
-
-echo  Doing dropping uninque index on params table - so we can add it next step
-mysql --batch -h localhost -u root --password=pass123 -D  BoilerControl  < droppin.txt > dsqlout.txt 
-
-echo  Add unique on param name
-mysql --batch -h localhost -u root --password=pass123 -D  BoilerControl  < addpun.txt > asqlout.txt 
-
-echo Insert row for new install - will fail if already there  
-mysql --batch -h localhost -u root --password=pass123 -D  BoilerControl  < insshut.txt > issqlout.txt    
-
-
-echo Doing update to ensure shutdown value is always N   - for when its not new
-mysql --batch -h localhost -u root --password=pass123 -D  BoilerControl  < upshut.txt > supout.txt    
-
-
-echo ################## Sleep installer #####################
-echo ##### We test for the existence of sleepdone file ######
-echo ############ if not there run scripts ################
-echo created 6/5/2016
-echo
-if ! [ -f sleepdone ]; then
-  echo 
-  echo The sleepdone file wasnt found, so run sql script to add tables and set new columns to zone
-  touch sleepdone
-  mysql --batch -h localhost -u root --password=pass123 -D  BoilerControl  < sqlsleep1.txt > sqlout.txt    
-else
-  echo The sleepdone file was found so its assumed sleep tables are installed already - skipping
-fi
-
-echo "##########################################################################"
-echo "########## Start the NOIP service to update fowarding service ############"
-echo "##########################################################################"
-echo starting noip
-cd /
-nohup sudo usr/local/bin/noip2 &
-cd /
-
-echo
-echo "##########################################################################"
-echo "################# Start the Control1 switching program ###################"
-echo "##########################################################################"
-cd /
-cd home/pi/led
-echo end of snooze
-nohup sudo python control1.py &
-cd /
-
-
-echo
-echo "##########################################################################"
-echo "############  Start the ProtoSen scanner job - Serial version ############"
-echo "##########################################################################"
-cd /
-cd home/pi/led
-nohup sudo python protosen.py &
-cd /
-
-
-
-echo
-echo "##########################################################################"
-echo "#########  Start the IP scanner job (one off run)- Serial version ########"
-echo "##########################################################################"
-
-cd /
-cd home/pi/led
-sudo python sendip.py 
-
-
-
-echo
-echo "##########################################################################"
-echo "################################# The end... #############################"
-echo "##########################################################################"
-cd /
+echo "Kicking kicker.sh"
+./home/pi/led/kicker.sh
