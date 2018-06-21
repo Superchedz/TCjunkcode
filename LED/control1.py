2a91c94f773b54f4a504cb54ce3020d9c122ac7c
Superchedz <grahamchedzoy@icloud.com>
2018-06-21 16:18:48 +0100

    fix
    indent errors

--- LED/control1.py
+++ LED/control1.py
@@ -640,18 +640,18 @@ def get_ip_address():
 #    return s.getsockname()[0]
 	
   s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    try:
+  try:
         # doesn't even have to be reachable
-      s.connect(('10.255.255.255', 1))
-      IP = s.getsockname()[0]
-      print "ip get routine"
-      print IP	  
-    except:
+    s.connect(('10.255.255.255', 1))
+    IP = s.getsockname()[0]
+    print "ip get routine"
+    print IP	  
+  except:
     print "error getting ip, defaulting to 127.0.0.1"
 	IP = '127.0.0.1'
-    finally:
+  finally:
       s.close()
-    return IP
+  return IP
   
 ###############################################################################################
 ###############################################################################################
