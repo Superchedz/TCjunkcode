a2130944a800b56bc84d61ae63221c460b065350
Superchedz <grahamchedzoy@icloud.com>
2019-01-27 22:46:12 +0000

    alexa checker
    automated check of the alexa job being running

---
+++ LED/checkalexa.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# This job checks if alexa is turned on, if it is, it checks the python job is running
+# if the job then isn't running, it emails
+
+pwfile=$(cat pwsf.txt)
+part=${pwfile:4:6}
+start="TC"
+end="9000"
+password="$start$part$end"
+myvar=$(mysql -u TCROOT9000 -p$password BoilerControl -NBse "SELECT * FROM params_b WHERE Param_Name = 'Alexa_YN'")
+Alexa_YN="$(echo $myvar | cut -d' ' -f2 )"
+
+if [ "$Alexa_YN" == "Y" ]
+then
+  echo "its on"
+  if pgrep -f "python TCAlexa.py" > /dev/null
+  then
+      echo "TCAlexa.py process is running"
+      exit
+  else
+      echo "TCAlexa.py process is not running - ohhh no"
+      sudo python /home/pi/led/alerter.py "Alerter  - TCAlexa Error" "Process TCAlexa.py was not found but param is on, check logs and restart"
+fi
