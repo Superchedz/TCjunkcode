73427d074791b9455a5f0ec8a5169dad50c1b4b7
Superchedz <grahamchedzoy@icloud.com>
2019-01-27 23:15:55 +0000

    Update getpw.py
    added full path

--- LED/getpw.py
+++ LED/getpw.py
@@ -1,6 +1,6 @@
 def getpass():
 
-  with open('pwsf.txt', 'r') as myfile:
+  with open('/home/pi/led/pwsf.txt', 'r') as myfile:
       data=myfile.read().replace('\n', '')
   password = "TC" + str(data[4:10]) + "9000"
   return password  
